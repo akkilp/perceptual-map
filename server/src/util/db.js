8da0a912b09939d430b5f49785f35d8a04e8465e
Aleksi Kilponen <akkilp@utu.fi>
2022-07-11 18:01:32 +0300

    rollback feature added for migrations

--- server/src/util/db.js
+++ server/src/util/db.js
@@ -2,8 +2,6 @@ const Sequelize = require('sequelize')
 const { DATABASE_URL } = require('./config')
 const { Umzug, SequelizeStorage } = require('umzug')
 
-const path = require("path")
-
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
@@ -14,19 +12,28 @@ const sequelize = new Sequelize(DATABASE_URL, {
   },
 });
 
+const migrationConf = {
+  migrations: {
+    glob: '**/migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
+
 const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: '**/migrations/*.js',
-    },
-    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  })
+  const migrator = new Umzug(migrationConf)
   const migrations = await migrator.up()
   console.log('Migrations up to date', {
     files: migrations.map((mig) => mig.name),
   })
+}
+
+const rollback = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+  console.log('Migrations rollbacked')
 }
 
 const connectToDatabase = async () => {
@@ -41,4 +48,4 @@ const connectToDatabase = async () => {
   return null
 }
 
-module.exports = { connectToDatabase, sequelize}
+module.exports = { connectToDatabase, sequelize, rollback}
