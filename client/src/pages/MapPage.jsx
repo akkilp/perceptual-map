762140abbdd1c2df00a0a5c78b2d85f9cc68e1a6
Aleksi Kilponen <akkilp@utu.fi>
2022-07-19 22:56:55 +0300

    qr code and share link added

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,8 +1,10 @@
 import { useNavigate, useParams } from 'react-router-dom'
-import { Container, Paper, Typography } from '@mui/material'
+import { Container, Paper, Grid, Typography, Button } from '@mui/material'
 
 import { useState } from 'react'
 import { fetchMap } from '../api_calls'
+
+import { useLocation } from 'react-router-dom'
 
 import useFetch from '../hooks/useFetch'
 import Loading from '../components/Loading'
@@ -10,10 +12,15 @@ import Loading from '../components/Loading'
 import SubjectList from '../components/SubjectList'
 import DimensionList from '../components/DimensionList'
 
+import QRCode from 'react-qr-code'
+import useMessager from '../hooks/useMessager'
+
 const MapPage = () => {
     let { mapId } = useParams()
     const [loading, error, data] = useFetch(() => fetchMap(mapId))
     const navigate = useNavigate()
+
+    const { displayMessage } = useMessager()
 
     if (loading)
         return (
@@ -60,72 +67,152 @@ const MapPage = () => {
         dateObject.getMonth() + 1
     }/${dateObject.getFullYear()}`
 
+    const copyToClipboard = (toCopy) => {
+        const el = document.createElement(`textarea`)
+        el.value = toCopy
+        el.setAttribute(`readonly`, ``)
+        el.style.position = `absolute`
+        el.style.left = `-9999px`
+        document.body.appendChild(el)
+        el.select()
+        document.execCommand(`copy`)
+        document.body.removeChild(el)
+    }
+
+    const answerUrl = `${window.location.href}/answer`
+
     return (
-        <Container
-            sx={{ my: 4, display: 'flex', flexDirection: 'column', gap: 3 }}
-        >
-            <Paper
-                sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                }}
-            >
-                <div>
-                    <Typography variant="h4">{title}</Typography>
-                    <Typography>
-                        Created by{' '}
-                        <span
+        <Container maxWidth="lg" sx={{ my: 4 }}>
+            <Grid container spacing={3} alignItems="stretch">
+                <Grid item xs={12}>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <div>
+                            <Typography variant="h4">{title}</Typography>
+                            <Typography>
+                                Created by{' '}
+                                <span
+                                    style={{
+                                        fontWeight: 500,
+                                        textDecoration: 'underline',
+                                        cursor: 'pointer',
+                                    }}
+                                    onClick={() =>
+                                        navigate(`/users/${createdBy.id}`)
+                                    }
+                                >
+                                    {createdBy.username}
+                                </span>
+                                {` `}
+                                {parsedDate}
+                            </Typography>
+                            <Typography>{description}</Typography>
+                        </div>
+                        <div
                             style={{
-                                fontWeight: 500,
-                                textDecoration: 'underline',
-                                cursor: 'pointer',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
                             }}
-                            onClick={() => navigate(`/users/${createdBy.id}`)}
                         >
-                            {createdBy.username}
-                        </span>
-                        {` `}
-                        {parsedDate}
-                    </Typography>
-                    <Typography>{description}</Typography>
-                </div>
-                <div
-                    style={{
+                            <Typography>Answered</Typography>
+                            <Typography variant="h4" component="p">
+                                {answers.length}
+                            </Typography>
+                            <Typography>times</Typography>
+                        </div>
+                    </Paper>
+                </Grid>
+
+                <Grid
+                    item
+                    sx={{
                         display: 'flex',
                         flexDirection: 'column',
-                        alignItems: 'center',
+                        gap: 3,
                     }}
+                    md={8}
+                    xs={12}
                 >
-                    <Typography>Answered</Typography>
-                    <Typography variant="h4" component="p">
-                        {answers.length}
-                    </Typography>
-                    <Typography>times</Typography>
-                </div>
-            </Paper>
-            <Paper
-                sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}
-            >
-                <DimensionList dimensions={dimensions} />
-            </Paper>
+                    <Paper
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-evenly',
+                            alignItems: 'center',
+                            p: 4,
+                            gap: 4,
+                        }}
+                    >
+                        <Button fullWidth size="large" variant="contained">
+                            Answer
+                        </Button>
+                        <Button fullWidth size="large" variant="contained">
+                            View
+                        </Button>
+                    </Paper>
+                    <Paper
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            flexGrow: 1,
+                        }}
+                    >
+                        <Button
+                            onClick={() => {
+                                copyToClipboard(answerUrl)
+                                displayMessage('Link copied!', 'success')
+                            }}
+                        >
+                            {answerUrl}
+                        </Button>
+                    </Paper>
+                </Grid>
 
-            <Paper
-                sx={{
-                    p: 2,
-                    pb: 3,
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}
-            >
-                <SubjectList subjects={subjects} />
-            </Paper>
+                <Grid item md={4} xs={12}>
+                    <Paper
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            py: 4,
+                        }}
+                    >
+                        <QRCode value={answerUrl} />
+                    </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <DimensionList dimensions={dimensions} />
+                    </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper
+                        sx={{
+                            p: 2,
+                            pb: 3,
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <SubjectList subjects={subjects} />
+                    </Paper>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
