57c4c9c26ce46eb6f495854a60ab2be4802badcb
Aleksi Kilponen <akkilp@utu.fi>
2022-07-29 15:26:30 +0300

    initialize anonymous user when server launching

---
+++ server/src/migrations/06-initialize-anonymous-user.js
@@ -0,0 +1,7 @@
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.sequelize.query(
+      "INSERT INTO users (id,username,email,password_hash,admin) VALUES (-1,'anonymous','doesnt@exist.com','asdasd',FALSE);"
+    );
+  },
+};
